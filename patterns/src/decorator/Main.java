37ddf6d7fad23ff3ec1857654a5da500eb50bdb5
rishabhkashyap <kashyap.rishab@hotmail.com>
2020-04-10 18:20:24 +0530

    Decorator pattern

--- patterns/src/decorator/Main.java
+++ patterns/src/decorator/Main.java
@@ -2,7 +2,7 @@ package patterns.src.decorator;
 
 public class Main {
     public static void main(String[] args){
-        Component ticket=new Header2(new Header1(new Footer1(new Footer3(new TicketPrinter()))));
+        Component ticket=new Header2(new Footer1(new Footer3(new TicketPrinter())));
         ticket.printTicket();
     }
 }
