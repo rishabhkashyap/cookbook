89749457a6a9b8cf1e10447c1e795f778af0881b
rishabhkashyap <kashyap.rishab@hotmail.com>
2021-02-14 17:59:59 +0530

    Examples of compose and andThen methods of Function interface

--- FunWithLambda/src/com/lambda/function/FunctionAndThenDemo.java
+++ FunWithLambda/src/com/lambda/function/FunctionAndThenDemo.java
@@ -4,9 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
-public class FunctionDemo {
-
-
+public class FunctionAndThenDemo {
     public static void main(String[] args) {
         List<Employee> employees = new ArrayList<>();
         employees.add(new Employee("Mike Ross", 30));
@@ -14,19 +12,14 @@ public class FunctionDemo {
         employees.add(new Employee("Dona Paulson", 40));
         employees.add(new Employee("Rachel Zane", 35));
         Function<Employee, String> firstName = (e) -> getFirstName(e);
-        List<String> firstNames = new ArrayList<>();
+        Function<String, Character> after = (e) -> e.charAt(0);
         for (Employee employee : employees) {
-            firstNames.add(firstName.apply(employee));
+            System.out.println(firstName.andThen(after).apply(employee));
         }
-        System.out.println(firstNames);
-
 
     }
 
     private static String getFirstName(Employee employee) {
         return employee.getName().split(" ")[0];
     }
-
-
 }
-
--- FunWithLambda/src/com/lambda/function/FunctionComposeDemo.java
+++ FunWithLambda/src/com/lambda/function/FunctionComposeDemo.java
@@ -4,29 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
-public class FunctionDemo {
-
-
+public class FunctionComposeDemo {
     public static void main(String[] args) {
         List<Employee> employees = new ArrayList<>();
         employees.add(new Employee("Mike Ross", 30));
         employees.add(new Employee("Harvey Specter", 45));
         employees.add(new Employee("Dona Paulson", 40));
         employees.add(new Employee("Rachel Zane", 35));
-        Function<Employee, String> firstName = (e) -> getFirstName(e);
-        List<String> firstNames = new ArrayList<>();
-        for (Employee employee : employees) {
-            firstNames.add(firstName.apply(employee));
+        Function<Employee, String> before = (e) -> getFirstName(e);
+        Function<String,Character> firstLetterOfName=(e)->e.charAt(0);
+        for (Employee employee:employees){
+            System.out.println(firstLetterOfName.compose(before).apply(employee));
         }
-        System.out.println(firstNames);
-
-
     }
-
     private static String getFirstName(Employee employee) {
         return employee.getName().split(" ")[0];
     }
-
-
 }
-
